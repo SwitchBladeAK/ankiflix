8544cbe3e8eb7f7f0b84158dc62af85477edfd96
Ankit Kashyap <ankitsonukashyap@gmail.com>
2023-12-30 10:51:19 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.scss";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { store } from "./store/store";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider store={store}>
         <App />
+        <SpeedInsights/>
     </Provider>
 );
